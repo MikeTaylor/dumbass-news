e47851300c1ce9b3664c9dffe7ac48e534fa57ef
Mike Taylor <mike@miketaylor.org.uk>
2022-01-19 13:01:57 +0000

    NewsServer object is curried into handler
    Instead of the config and logger objects. The server contains both of
    these, as well as the soon-to-be-introduced HTTP client.

--- src/server.go
+++ src/server.go
@@ -5,14 +5,14 @@ import "fmt"
 import "strings"
 import "time"
 
-func showChannel(w http.ResponseWriter, config *Config, logger *Logger, channel string, transformation string) {
-	channelConfig, ok := config.Channels[channel]
+func showChannel(w http.ResponseWriter, server *NewsServer, channel string, transformation string) {
+	channelConfig, ok := server.config.Channels[channel]
 	if !ok {
 		w.WriteHeader(http.StatusBadRequest)
 		fmt.Fprintln(w, "unknown channel:", channel)
 		return
 	}
-	logger.log("config", fmt.Sprintf("%+v", channelConfig))
+	server.logger.log("config", fmt.Sprintf("%+v", channelConfig))
 	ctype := channelConfig.ChannelType
 	if ctype != "rss" {
 		w.WriteHeader(http.StatusBadRequest)
@@ -22,17 +22,17 @@ func showChannel(w http.ResponseWriter, config *Config, logger *Logger, channel 
 	fmt.Fprintln(w, "channel:", channel, "- transformation:", transformation)
 }
 
-func handler(w http.ResponseWriter, req *http.Request, config *Config, logger *Logger) {
+func handler(w http.ResponseWriter, req *http.Request, server *NewsServer) {
 	raw := strings.Split(req.URL.Path, "/")
 	chunks := raw[1:] // Skip initial empty component
-	logger.log("path", strings.Join(chunks, "/"))
+	server.logger.log("path", strings.Join(chunks, "/"))
 
 	if len(chunks) == 1 && chunks[0] == "" {
 		// Home page
 		fmt.Fprintln(w, `<a href="/bbc/dumbass">Example</a>`)
 	} else if len(chunks) == 2 {
 		// Transformed channel
-		showChannel(w, config, logger, chunks[0], chunks[1])
+		showChannel(w, server, chunks[0], chunks[1])
 	} else {
 		// Unrecognized
 		w.WriteHeader(http.StatusNotFound)
@@ -56,7 +56,7 @@ func MakeNewsServer(config *Config, logger *Logger) *NewsServer {
 	}
 
 	// XXX I would prefer this to be registered only to server.server
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) { handler(w, r, config, logger) })
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) { handler(w, r, &server) })
 
 	return &server
 }
