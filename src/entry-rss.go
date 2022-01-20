716509b86cabd1a79524b3860e935ec7f03d080d
Mike Taylor <mike@indexdata.com>
2022-01-20 17:30:16 +0000

    Parsing RSS sufficiently correctly

--- src/entry-rss.go
+++ src/entry-rss.go
@@ -1,6 +1,7 @@
 package main
 
 import "fmt"
+import "errors"
 import "encoding/xml"
 
 type item struct {
@@ -17,8 +18,8 @@ type channel struct {
 }
 
 type rss struct {
-	XMLName  xml.Name `xml:"rss"`
-	Channels channel  `xml:"channel"`
+	XMLName  xml.Name  `xml:"rss"`
+	Channels []channel `xml:"channel"`
 }
 
 func makeRssEntries(body []byte) ([]Entry, error) {
@@ -30,9 +31,23 @@ func makeRssEntries(body []byte) ([]Entry, error) {
 		return nil, fmt.Errorf("cannot parse RSS: %w", err)
 	}
 
-	fmt.Println("parse err:", err, "doc:", doc)
+	// XXX Not sure at the moment what to do with top-level
+	// metadata. For the time being I will just discard it, along
+	// with all channels but the first; and merge the item title
+	// and description into a single field.
 
-	return nil, nil
+	if doc.Channels == nil || len(doc.Channels) == 0 {
+		return nil, errors.New("no channels in RSS feed")
+	}
+	ch := doc.Channels[0]
+	res := make([]Entry, len(ch.Items))
+	for i := 0; i < len(ch.Items); i++ {
+		item := ch.Items[i]
+		res[i].Headline = item.Title + " -- " + item.Description
+		res[i].Link = item.Link
+	}
+
+	return res, nil
 }
 
 var RssEntryParser = EntryParser{
