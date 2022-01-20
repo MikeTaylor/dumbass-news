6a606d6f76933d7ca1f5d9355dd50e5e8dcacd76
Mike Taylor <mike@indexdata.com>
2022-01-20 17:02:33 +0000

    Some actual parsing of RSS

--- src/entry-rss.go
+++ src/entry-rss.go
@@ -3,20 +3,29 @@ package main
 import "fmt"
 import "encoding/xml"
 
+type item struct {
+	Title       string `xml:"title"`
+	Description string `xml:"description"`
+	Link        string `xml:"link"`
+}
+
 type channel struct {
-	title string `xml:"title"`
+	Title       string `xml:"title"`
+	Description string `xml:"description"`
+	Link        string `xml:"link"`
+	Items       []item `xml:"item"`
 }
 
 type rss struct {
 	XMLName  xml.Name `xml:"rss"`
-	channels channel  `xml:"channel"`
+	Channels channel  `xml:"channel"`
 }
 
 func makeRssEntries(body []byte) ([]Entry, error) {
 	fmt.Println("in makeRssEntries")
 
 	var doc rss
-	err := xml.Unmarshal([]byte("xxx"), &doc)
+	err := xml.Unmarshal([]byte(body), &doc)
 	if err != nil {
 		return nil, fmt.Errorf("cannot parse RSS: %w", err)
 	}
