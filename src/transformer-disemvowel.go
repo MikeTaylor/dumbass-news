bcc674530072d61c46a2aef0462a1a1377f1233a
Mike Taylor <mike@miketaylor.org.uk>
2022-01-25 11:33:30 +0000

    Inline the disemvowelTransform function

--- src/transformer-disemvowel.go
+++ src/transformer-disemvowel.go
@@ -4,15 +4,13 @@ import "regexp"
 
 var re *regexp.Regexp
 
-func disemvowelTransform(entry *Entry) error {
-	if re == nil {
-		re = regexp.MustCompile("[aeiou]")
-	}
-
-	entry.Headline = re.ReplaceAllString(entry.Headline, "")
-	return nil
-}
-
 var DisemvowelTransformer = Transformer{
-	transform: disemvowelTransform,
+	transform: func(entry *Entry) error {
+		if re == nil {
+			re = regexp.MustCompile("[aeiou]")
+		}
+
+		entry.Headline = re.ReplaceAllString(entry.Headline, "")
+		return nil
+	},
 }
