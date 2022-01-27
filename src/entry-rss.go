a09b12b63a2e8dab29888b04aff2d0dbcbf68caf
Mike Taylor <mike@miketaylor.org.uk>
2022-01-27 17:20:55 +0000

    Move named function makeRssEntries to anonymous in struct

--- src/entry-rss.go
+++ src/entry-rss.go
@@ -22,38 +22,36 @@ type rss struct {
 	Channels []channel `xml:"channel"`
 }
 
-func makeRssEntries(cc channelConfig, body []byte) ([]Entry, error) {
-	var doc rss
-	err := xml.Unmarshal([]byte(body), &doc)
-	if err != nil {
-		return nil, fmt.Errorf("cannot parse RSS: %w", err)
-	}
-
-	// XXX Not sure at the moment what to do with top-level
-	// metadata. For the time being I will just discard it, along
-	// with all channels but the first; and merge the item title
-	// and description into a single field.
-
-	if doc.Channels == nil || len(doc.Channels) == 0 {
-		return nil, errors.New("no channels in RSS feed")
-	}
-	ch := doc.Channels[0]
-	res := make([]Entry, len(ch.Items))
-	for i := 0; i < len(ch.Items); i++ {
-		item := ch.Items[i]
-		if cc.Render == "title" {
-			res[i].Headline = item.Title
-		} else if cc.Render == "description" {
-			res[i].Headline = item.Description
-		} else {
-			res[i].Headline = item.Title + " -- " + item.Description
-		}
-		res[i].Link = item.Link
-	}
-
-	return res, nil
-}
-
 var RssEntryParser = EntryParser{
-	parse: makeRssEntries,
+	parse: func(cc channelConfig, body []byte) ([]Entry, error) {
+		var doc rss
+		err := xml.Unmarshal([]byte(body), &doc)
+		if err != nil {
+			return nil, fmt.Errorf("cannot parse RSS: %w", err)
+		}
+
+		// XXX Not sure at the moment what to do with top-level
+		// metadata. For the time being I will just discard it, along
+		// with all channels but the first; and merge the item title
+		// and description into a single field.
+
+		if doc.Channels == nil || len(doc.Channels) == 0 {
+			return nil, errors.New("no channels in RSS feed")
+		}
+		ch := doc.Channels[0]
+		res := make([]Entry, len(ch.Items))
+		for i := 0; i < len(ch.Items); i++ {
+			item := ch.Items[i]
+			if cc.Render == "title" {
+				res[i].Headline = item.Title
+			} else if cc.Render == "description" {
+				res[i].Headline = item.Description
+			} else {
+				res[i].Headline = item.Title + " -- " + item.Description
+			}
+			res[i].Link = item.Link
+		}
+
+		return res, nil
+	},
 }
