37f578000664272dff23a12d9f8d0e97cc38cfe7
Mike Taylor <mike@miketaylor.org.uk>
2022-01-31 16:35:36 +0000

    Add go-doc comments

--- src/categorical-logger/logger.go
+++ src/categorical-logger/logger.go
@@ -1,11 +1,10 @@
-// logger is derived from the JavaScript package categorical-logger,
-// which can be found at
+// categorical-logger/clogger is derived from the JavaScript package
+// categorical-logger, which can be found at
 // https://github.com/openlibraryenvironment/categorical-logger
-// The present version of logger.go falls short of its ancestor in the
-// following respects:
-// 1. It does not support logging functional arguments
-// 2. It does not have the the setter methods
-// 3. It is not documented
+// The present version of this library falls short of its ancestor in
+// two respects:
+//  1. It does not support logging functional arguments
+//  2. It does not have the the setter methods
 //
 package clogger
 
@@ -14,6 +13,11 @@ import "time"
 import "fmt"
 import "os"
 
+// Logger is an opaque structure created by MakeLogger and which
+// encapsulates the configuration passed into that function. Typically
+// a program will make just one of these, and pass it around as
+// necessary.
+//
 type Logger struct {
 	categories string
 	prefix     string
@@ -32,6 +36,23 @@ func getCategories(fallback string) string {
 	return fallback
 }
 
+// MakeLogger creates a categorical-logger object on which the Log
+// method may subsequently be called. The logger is configured by
+// three parameters:
+//
+// categories: a string containing zero or more comma-separated
+// logging categories, such as "init,config,url", for which the logger
+// should emit messages. There is no predefined list of categegories,
+// and no hierarchy between categories -- unlike the FATAL, ERROR,
+// WARN, INFO, DEBUG hierarchy in log4j and similar
+// libraries. Applications can use whatever categories they wish.
+//
+// prefix: a short string or nil. If the former, then it is included
+// at the start of each log message.
+//
+// timestamp: a boolean indicating whether or not a timestamp is to be
+// included in each log message
+//
 func MakeLogger(categories string, prefix string, timestamp bool) *Logger {
 	var logger Logger
 
@@ -42,12 +63,22 @@ func MakeLogger(categories string, prefix string, timestamp bool) *Logger {
 	return &logger
 }
 
+// HasCategory returns true is the logger has been configured to
+// include the named category and false if not.
+//
 func (l *Logger) HasCategory(cat string) bool {
 	var xlist string = "," + l.categories + ","
 	var xcat string = "," + cat + ","
 	return strings.Contains(xlist, xcat)
 }
 
+// Log does nothing if the named category is not among those that the
+// logger is configured to use. But if the logged is configured to
+// include the category, then it logs a message to standard error,
+// consisting of the logger prefix (if defined), a timestamp (if
+// configured), and all the supplied strings, separated by spaces and
+// terminated by a newline.
+//
 func (l *Logger) Log(cat string, args ...string) {
 	if !l.HasCategory(cat) {
 		return
